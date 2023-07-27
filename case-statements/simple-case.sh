71593dc83b4ac6c2d576f3c6db4ae630fc28cf19
manu <manu@manu-vertualbox@.com>
2023-07-27 09:31:11 +0530

    simple case statements example

---
+++ case-statements/simple-case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "You can choose numbers between 1 to 10 only"
+echo "Enter number"
+read num
+case $num in
+	1) echo " You enter a number is 1 " ;;
+	2) echo " You enter a number is 2 " ;;
+	3) echo " You enter a number is 3 " ;;
+	4) echo " You enter a number is 4 " ;;
+	5) echo " You enter a number is 5 " ;;
+	6) echo " You enter a number is 6 " ;;
+	7) echo " You enter a number is 7 " ;;
+	8) echo " You enter a number is 8 " ;;
+	9) echo " You enter a number is 9 " ;;
+	10) echo " You enter a number is 10 " ;;
+esac
+	
