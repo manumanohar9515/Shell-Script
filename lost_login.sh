2b6ce54e76d39022f6ecb418436d068614d486ca
manu <manu@manu-vertualbox@.com>
2023-08-02 13:22:21 +0530

    modification

--- lost_login.sh
+++ lost_login.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ $# -lt 1 ]
 then
-	echo "Please provide salid input"
+	echo "Provide salid string"
 	exit 255
 fi
 PASSWD=$1
 for SERVER in `cat servers-list`
 do
-	sshpass -p $PASSWD ssh root@$SERVER "echo -e HOstname:$SERVER; last -w | awk '{print $1, $5, $6}'" >>/tmp/last_log
+        sshpass -p $PASSWD ssh test1@$SERVER "echo -e Hostname:$SERVER; last -w " >>/home/manu/last_login
 done
 
